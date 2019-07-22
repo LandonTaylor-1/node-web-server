c4c05967443bc0a89ff07f3e32c5ab0296015378
LanodnTaylor-1 <landon.autumn.taylor@gmail.com>
2019-07-22 13:52:11 -0500

    Landon Taylor

--- index.js
+++ index.js
@@ -1,30 +1,66 @@
 var http = require('http');
 let users = require("./state").users;
+//let newUser = require("./newUser").newUser;
 let server = http.createServer(messageReceived);
 
-server.listen(80);
+server.listen(8080);
 
 function messageReceived(req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
-   let usersJSON = JSON.stringify(users);
-   res.write(usersJSON);
-    if(req.method === "GET" && req.url.indexOf("/users/") > -1){
-        let id = req.url.split("/");
-        let user = users.find(p=>p["_id"] == id[2]);
-        let userJSON = JSON.stringify(user);
-        res.write(userJSON);
-    }
-    else if(req.method === "POST" && req.url === "/products"){
-        res.write("you wanted to make a product")
-    }
-    else if(req.method === "PUT" && req.url === "/products"){
-    res.write("you wanted to update a product")
-    }
-    else if(req.method === "DELETE" && req.url === "/products"){
-    res.write("you wanted to delete a product")
-    }
-    else{
-    res.write("Not Found");
+    if(req.method === "GET" && req.url === "/users"){
+      let usersJSON = JSON.stringify(users);
+      res.write(usersJSON);
+    }else if(req.method === "GET" && req.url.indexOf("/users/") > -1){
+      let id = req.url.split("/");
+      let user = users.find(p=>p["_id"] == id[2]);
+      let userJSON = JSON.stringify(user);
+      res.write(userJSON);
+    }else if(req.method === "POST" && req.url === "/users"){
+      postUsers(req, res);
+    // }else if(req.method === "PUT" && req.url.indexOf("/users/") > -1){
+    //   let id = req.url.split("/");
+    //   let user = users.find(p=>p["_id"] == id[2]);
+    //   let userJSON = JSON.stringify(user);
+    //   userJSON = [];
+    //   let body = userJSON
+    //   req.on('data', (chunk) => {
+    //     body.push(chunk);
+    //   }).on('end', () => {
+    //     body = Buffer.concat(body).toString();
+    //     let changedUser = JSON.parse(body);
+    //     user.push(changedUser);
+    //   });
+    //   //putUsers(req, res);
+    }else if(req.method === "DELETE" && req.url.indexOf("/users/") > -1){
+      let id = req.url.split("/");
+      let user = users.find(p=>p["_id"] == id[2]);
+      users.splice(user, 1)
+    }else{
+      res.write("Not Found");
     }
   res.end();
 }
+
+function postUsers(request, response){
+   let body = [];
+   request.on('data', (chunk) => {
+     body.push(chunk);
+   }).on('end', () => {
+     body = Buffer.concat(body).toString();
+     let newUser = JSON.parse(body);
+     users.push(newUser);
+   });
+  }
+
+  // function putUsers(request, response){
+  //   let id = request.url.split("/");
+  //   let user = users.find(p=>p["_id"] == id[2]);
+  //   let body = [];
+  //   request.on('data', (chunk) => {
+  //     body.push(chunk);
+  //   }).on('end', () => {
+  //     body = Buffer.concat(body).toString();
+  //     let changedUser = JSON.parse(body);
+  //     user.push(changedUser);
+  //   });
+  //  }
---
+++ newUser.js
@@ -0,0 +1,6 @@
+exports.newUser = {
+    "_id": 6,
+    "name": "Erin Aaron",
+    "occupation": "Web Developer",
+    "avatar": "https://www.bluecross.org.uk/sites/default/files/assets/images/124044lpr.jpg"
+}
